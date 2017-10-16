73e308535e2131afc00ae34e2cf6981c0e39c3eb
anhmaker <anhmaker@xrm.ru>
2017-10-17 02:09:43 +0500

    blog-comet

--- Blog/ClientApp/src/app/chat/chat.module.ts
+++ Blog/ClientApp/src/app/chat/chat.module.ts
@@ -1,8 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { VideoChat } from './video-chat/video-chat.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [VideoChat],
   exports: [VideoChat],
   providers: [],
--- Blog/ClientApp/src/app/chat/video-chat/video-chat.component.ts
+++ Blog/ClientApp/src/app/chat/video-chat/video-chat.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+declare const navigator;
 
 @Component({
   selector: 'video-chat',
   templateUrl: './video-chat.component.html',
   styleUrls: ['./video-chat.component.css']
 })
-export class VideoChat{
-  constructor(){
+export class VideoChat implements OnInit {
+  public cameraOn: boolean;
+  public broadcastOn: boolean = false;
+  public userMedia = <any>navigator;
+  public streamObject: any;
+  @ViewChild('videoElem') videoElem: any;
+  public currentStreamId: any;
 
+  constructor() {
+    this.cameraOn = false;
   }
 
+  ngOnInit() {
+    this.getCamera();
+    this.cameraOn = true;
+  }
 
+  getCamera() {
+    if (!navigator.getUserMedia)
+      this.userMedia.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    if (!navigator.cancelAnimationFrame)
+      this.userMedia.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+    if (!navigator.requestAnimationFrame)
+      this.userMedia.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+    this.userMedia.getUserMedia(
+      {
+        audio: {
+          "mandatory": {
+            "googEchoCancellation": "false",
+            "googAutoGainControl": "false",
+            "googNoiseSuppression": "false",
+            "googHighpassFilter": "false"
+          },
+          "optional": []
+        }, video: true
+      }, (stream) => this.streamVideo(stream), (e) => this.error(e));
+  }
+  streamVideo(remoteStream) {
+    this.streamObject = remoteStream;
+    this.videoElem.nativeElement.srcObject = remoteStream;
+    console.log("stream", remoteStream);
+  }
+  error(err) {
+    console.log(err);
+  }
+  broadcast() {
+    this.broadcastOn = true;
+  }
+
+  stopBroadcast() {
+    this.broadcastOn = false;
+  }
 }
